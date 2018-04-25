bb50af434ee4795d500ff176d41b96a8b7b5a9c4
hjc <hjc@163.com>
2018-04-25 18:02:00 +0800

    提交filter过滤器，请求过滤，返回过滤不？？？

--- src/main/java/com/hjc/filter/MyFilter.java
+++ src/main/java/com/hjc/filter/MyFilter.java
@@ -7,16 +7,19 @@ public class MyFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        System.out.println("filter init");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-
+        System.out.println("filter In--"+servletRequest.getParameter("id"));
+        System.out.println("filter doFilter");
+        System.out.println(servletResponse.getContentType());
+        filterChain.doFilter(servletRequest,servletResponse);
     }
 
     @Override
     public void destroy() {
-
+        System.out.println("filter destroy");
     }
 }
